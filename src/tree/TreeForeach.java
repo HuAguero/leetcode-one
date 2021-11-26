dec9cff5e2393a2f8d73d138748939d84f96ac7f
Hu Aguero <huguiaguero@outlook.com>
2021-11-26 22:03:40 +0800

    middle order and after order foreach.

--- src/tree/TreeForeach.java
+++ src/tree/TreeForeach.java
@@ -1,5 +1,9 @@
 package tree;
 
+import sun.reflect.generics.tree.Tree;
+
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+
 /*
 二叉树遍历
 前序遍历：根左右
@@ -22,12 +26,14 @@ public class TreeForeach {
         TreeNode node2 = new TreeNode(2, node4, node5);
         TreeNode node1 = new TreeNode(1, node2, node3);
 
-        preOrderRootLeftRight(node1);
+        //preOrderRootLeftRight(node1);
+        //midOrderLeftRootRight(node1);
+        afterOrderLeftRightRoot(node1);
     }
 
     //前序遍历
-    public static void preOrderRootLeftRight(TreeNode root){
-        if(root == null)
+    public static void preOrderRootLeftRight(TreeNode root) {
+        if (root == null)
             return;
 
         System.out.println(root.val);//前序：第一次成为栈顶打印
@@ -36,7 +42,28 @@ public class TreeForeach {
         preOrderRootLeftRight(root.right);
     }
 
+    //中序遍历
+    public static void midOrderLeftRootRight(TreeNode root) {
+        if (root == null) {
+            return;
+        }
 
+        midOrderLeftRootRight(root.left);
+        System.out.println(root.val);
+        midOrderLeftRootRight(root.right);
+    }
+
+
+    //后序遍历
+    public static void afterOrderLeftRightRoot(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        afterOrderLeftRightRoot(root.left);
+        afterOrderLeftRightRoot(root.right);
+        System.out.println(root.val);
+    }
 
 
 
